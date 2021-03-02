3ab2bd25b741e22c4444c76285f07f44d8aab767
Kristjan Topić <kris.topic@gmail.com>
2021-03-02 23:37:12 +0100

    Napravljen navbar i layot home stranice

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,62 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light sticky-top">
+      <!-- Image and text -->
+      
+        <a class="navbar-brand" href="#">
+          <img
+            src="@/assets/logo-antic.png"
+            alt="DietLife-logo"
+            height="50"
+            class="d-inline-block align-top"
+          />
+        </a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="nav-buttons">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link
+                to="/"
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+                >Home</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link" href="#"
+                >About</router-link
+              >
+            </li>
+          </ul>
+          </div>
+          
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search recipes"
+              aria-label="Search"
+            />
+          </form>
+        </div>
+          
+    </nav>
+    <router-view />
+    
   </div>
 </template>
 
@@ -13,12 +65,14 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  padding: 10px;
+  background-color: white;
+  min-width: 400px;
 
   a {
     font-weight: bold;
@@ -28,5 +82,9 @@
       color: #42b983;
     }
   }
+  .nav-buttons {
+    padding: 12px;
+  }
+  
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    style="margin-top:0px"
+    class="row row-cols-auto g-4 justify-content-center"
+  >
+    
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+      <recipe-card/>
+
+
+      
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  import RecipeCard from '@/components/RecipeCard.vue';
+  export default {
+    name: 'home',
+    components: {
+      RecipeCard
+    },
+  };
 </script>
