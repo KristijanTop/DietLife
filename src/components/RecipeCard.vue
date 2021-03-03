160f3618927dbbcced49cc20058da17d5f5d8b95
Kristjan Topić <kris.topic@gmail.com>
2021-03-03 02:26:47 +0100

    Dodavanje RecipeCard komponente

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col">
+    <div style="width:400px" class="card">
+      <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          This is a longer card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecipeCard",
+};
+</script>
